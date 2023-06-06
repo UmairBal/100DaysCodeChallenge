58e9bdabc5bed23c3bbc1219602464c96ce6aad7
UmairBal <86747265+UmairBal@users.noreply.github.com>
2023-06-06 10:49:26 -0400

    Update Day13_Patterns.py

--- Day13_Patterns.py
+++ Day13_Patterns.py
@@ -1,6 +1,21 @@
+################# Inverted Pattern ##################
+
 n = int(input("Enter a number: "))
 for i in range(n):
     for j in range(1, n+1-i):
         print(j, end=" ")
     print()
+
+################## 0-1 Pattern #####################
+
+n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    for j in range(i):
+        if (i + j) % 2 == 0:
+            print("1", end=" ")
+        else:
+            print("0", end=" ")
+    print()
+
+
 
