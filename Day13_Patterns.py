edfb2767bdd4fea662af80df43c4d5dc6962f86c
UmairBal <86747265+UmairBal@users.noreply.github.com>
2023-06-06 11:01:03 -0400

    Update Day13_Patterns.py

--- Day13_Patterns.py
+++ Day13_Patterns.py
@@ -8,7 +8,6 @@ for i in range(n):
 
 ################## 0-1 Pattern #####################
 
-n = int(input("Enter a number: "))
 for i in range(1, n + 1):
     for j in range(i):
         if (i + j) % 2 == 0:
@@ -19,7 +18,6 @@ for i in range(1, n + 1):
     
 ################## Rhombus pattern  #####################
 
-n = int(input("Enter a number: "))
 for i in range(n):
     for j in range(1,n+1-i):
         print(" ", end=" ")
