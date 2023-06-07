b09ae652b4f289876900a92bfccb2a891f4dc2d6
UmairBal <86747265+UmairBal@users.noreply.github.com>
2023-06-07 17:02:53 -0400

    Update Day14_AdvancedPatterns.py

--- Day14_AdvancedPatterns.py
+++ Day14_AdvancedPatterns.py
@@ -1,3 +1,5 @@
+##################### Star Pattern ######################
+
 n = int(input("Enter a number "))
 for i in range(1, n):
     for j in range(1, n - i + 1):
@@ -12,3 +14,14 @@ for i in range(1, n):
     for k in range(1, n - i + 1):
         print("* ", end="")
     print()
+
+##################### Zig Zag Pattern #####################
+
+for i in range(1, 4):
+    for j in range(1, n+1):
+        if (((i+j) % 4) == 0) or (i == 2 and j % 4 == 0):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
