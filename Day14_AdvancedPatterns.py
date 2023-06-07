2f58edf746f75ed2d6ca7739643c731557b06f98
UmairBal <86747265+UmairBal@users.noreply.github.com>
2023-06-07 17:04:16 -0400

    Update Day14_AdvancedPatterns.py

--- Day14_AdvancedPatterns.py
+++ Day14_AdvancedPatterns.py
@@ -20,8 +20,8 @@ for i in range(1, n):
 for i in range(1, 4):
     for j in range(1, n+1):
         if (((i+j) % 4) == 0) or (i == 2 and j % 4 == 0):
-            print("*", end="")
+            print("* ", end="")
         else:
-            print(" ", end="")
+            print("  ", end="")
     print()
 
